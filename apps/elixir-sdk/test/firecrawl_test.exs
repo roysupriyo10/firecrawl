4ba00932f6e6ceee4aa6f44089abe126cfe97748
Abimael Martell <abimex@gmail.com>
2026-06-01 14:23:40 -0700

    Add redactPII to SDK options

--- apps/elixir-sdk/test/firecrawl_test.exs
+++ apps/elixir-sdk/test/firecrawl_test.exs
@@ -215,6 +215,41 @@ defmodule FirecrawlTest do
     assert {:ok, %Req.Response{status: 200}} = result
   end
 
+  test "scrape maps redact_pii to redactPII" do
+    parent = self()
+
+    adapter = fn request ->
+      send(parent, {:request, request})
+
+      resp = Req.Response.new(
+        status: 200,
+        headers: %{"content-type" => ["application/json"]},
+        body: Jason.encode!(%{"success" => true, "data" => %{}})
+      )
+
+      {request, resp}
+    end
+
+    assert {:ok, %Req.Response{status: 200}} =
+             Firecrawl.scrape_and_extract_from_url(
+               [url: "https://example.com", redact_pii: true],
+               api_key: "test-key",
+               adapter: adapter
+             )
+
+    assert_receive {:request, request}
+
+    body =
+      cond do
+        is_binary(request.body) -> Jason.decode!(request.body)
+        is_map(request.body) -> request.body
+        true -> request.options[:json]
+      end
+
+    assert body["redactPII"] == true
+    refute Map.has_key?(body, "formats")
+  end
+
   test "all expected API functions are defined with bang variants" do
     functions = Firecrawl.__info__(:functions)
 
